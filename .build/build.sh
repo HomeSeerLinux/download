b3da884ba696eec222c1b2fa3563b5b03b04b736
Robert Savage <robert@savagehomeautomation.com>
2022-05-04 10:02:38 -0400

    added [4.2.11.3-beta] and [4.2.12] releases

--- .build/build.sh
+++ .build/build.sh
@@ -7,9 +7,9 @@ set -e
 cp ~/.gnupg/homeseer.key .   # copy local homeseer PGP private key to to use during build
 
 # build HOMESEER.SH debian installer packages
-docker run --rm --volume $(pwd):/build pi4j/pi4j-builder-repo:latest .build/build-debs.sh
+docker run --rm --platform linux/amd64  --volume $(pwd):/build pi4j/pi4j-builder-repo:latest .build/build-debs.sh
 
 # build HOMESEER.SH [V4] distribution APT repository metadata
-docker run --rm --volume $(pwd):/build pi4j/pi4j-builder-repo:latest .build/build-repo-v4.sh
+docker run --rm --platform linux/amd64 --volume $(pwd):/build pi4j/pi4j-builder-repo:latest .build/build-repo-v4.sh
 
 rm homeseer.key              # remove local copy of homeseer PGP private key
